95737bad692043e963b88f17a041061f0b4accfe
Luis Henriquez-Perez <luis@luishp.xyz>
2021-03-27 17:14:52 -0400

    Add cache variable.

--- tangld.el
+++ tangld.el
@@ -105,6 +105,11 @@ during init"
   :group 'tangld
   :type 'boolean)
 
+(defcustom tangld-cache-org-babel-library-p t
+  "Non-nil if should cache."
+  :group 'tangld
+  :type boolean)
+
 ;;;; Initialization - tangld-init
 
 (defun tangld-init ()
