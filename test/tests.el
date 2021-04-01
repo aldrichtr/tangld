84dde77237f99b12048071f4880391e06801c898
Luis Henriquez-Perez <luis@luishp.xyz>
2021-03-31 22:38:17 -0400

    Update.

--- test/tests.el
+++ test/tests.el
@@ -1,7 +1,7 @@
 ;; The tangld interactive functions we want to test are full of side-effects
 ;; like creating files, and directories. It easier and safer to test these
 ;; commands if they'd only return values instead of creating/deleting files and
-;; directories. 
+;; directories. Thankfully, we can use advice.
 
 (ert-deftest tangld--expanded-project-dir-paths ()
   (should (tangld::expanded-project-dir-paths ))
