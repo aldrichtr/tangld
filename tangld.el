912dc32e62938aca11ef286ea71995982ab1f0eb
Luis Henriquez-Perez <luis@luishp.xyz>
2021-04-04 19:43:55 -0400

    Update tangld

--- tangld.el
+++ tangld.el
@@ -274,8 +274,7 @@ By default, build will only tangle files that have changed since last run."
     (f-symlink file target)))
 
 (defun tangld--link-type-stow-build (file)
-  "Invoke stow to."
-  ())
+  "Invoke stow to.")
 
 ;; (defun tangld--link-type-direct-build (file))
 
