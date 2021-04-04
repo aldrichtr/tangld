0ce2fd874db91aa0f669a8d47b6898ddec19be79
Luis Henriquez-Perez <luis@luishp.xyz>
2021-04-04 19:18:38 -0400

    Update tangld

--- tangld.el
+++ tangld.el
@@ -283,7 +283,7 @@ By default, build will only tangle files that have changed since last run."
 ;;;; Clean - tangld-clean
 
 (defun tangld-clean ()
-  "Remove any symlinks "
+  "Remove any symlinks corresponding to files in dotfiles-dir."
   (interactive)
   (let ((dotfiles-dir (alist-get 'dotfiles tangld-project-dirs))
 	(system-dir (alist-get 'system tangld-project-dirs))
