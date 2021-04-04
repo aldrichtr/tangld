debce0c1661325419649749c86ca0b9f6ac0590f
Luis Henriquez-Perez <luis@luishp.xyz>
2021-04-04 19:45:06 -0400

    Update tangld

--- tangld.el
+++ tangld.el
@@ -284,15 +284,8 @@ By default, build will only tangle files that have changed since last run."
   "Symlink files in dotfiles directory to system directory."
   (interactive)
   (run-hooks 'tangld-pre-install-hook)
-  (let* ((dotfiles-dir (alist-get 'dotfiles tangld-project-dirs))
-	 (system-dir (alist-get 'system tangld-project-dirs))
-	 (files (directory-files-recursively dotfiles-dir "."))
-	 (relative-path nil)
-	 (target-path nil))
-    (dolist (file files)
-      (setq relative-path (f-relative file dotfiles-dir))
-      (setq target-path (f-expand relative-path system-dir))
-      (f-symlink file target-path)))
+  (let* ((files (directory-files-recursively dotfiles-dir ".")))
+    (dolist (file files) (tangld--link-type-link-build file)))
   (run-hooks 'tangld-post-install-hook))
 
 ;;;; Clean - tangld-clean
