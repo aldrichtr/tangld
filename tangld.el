c6ec75c50a43e44ee40049ea0275c4a63496a8d5
Luis Henriquez-Perez <luis@luishp.xyz>
2021-04-04 19:31:59 -0400

    Update tangld

--- tangld.el
+++ tangld.el
@@ -215,26 +215,26 @@ during init"
 ;;;; Configuration - tangld-config
 
 (defun tangld-config (&optional type)
-  "Configure tangld. 
+          "Configure tangld. 
 
 Set the source and target directories, Gather system information, and store for
 the build step to use. If TYPE is specified store as options for a specific
 build type i.e. OS specific, shell options alternate install directory, etc."
-  (interactive)
-  ;; The user sets options such as cache use, source and target dirs, etc.
-  ;; write those to a project specific configuration location so that build
-  ;; can use them as it's config when run.
+          (interactive)
+          ;; The user sets options such as cache use, source and target dirs, etc.
+          ;; write those to a project specific configuration location so that build
+          ;; can use them as it's config when run.
 
-  ;; environmental details
-  ;; (list system-type system-name user-full-name)
-  (tangld--message "environment details: %s %s %s" system-type system-name user-full-name)
+          ;; environmental details
+          ;; (list system-type system-name user-full-name)
+          (tangld--message "environment details: %s %s %s" system-type system-name user-full-name)
 
-  ;; tangld options
-  (tangld--message "tangled-options: %s %s %s" "uh" "don't" "know?")
-  ;; language, tangle options, exclusions, overrides,
+          ;; tangld options
+          (tangld--message "tangled-options: %s %s %s" "uh" "don't" "know?")
+          ;; language, tangle options, exclusions, overrides,
 
-  ;; write-config
-  (let-alist tangld-project-dirs
+          ;; write-config
+          (let-alist tangld-project-dirs
     (let ((config-file (f-join .root .lib tangld-config-file)))
       (tangld--ignore (f-write config-file))
       (tangld--message "Build options saved to '%s'" config-file))))
@@ -242,12 +242,12 @@ build type i.e. OS specific, shell options alternate install directory, etc."
 ;;;; Build - tangld-build
 
 (defun tangld-build (&optional force)
-  "Tangle org-mode files from the source dir to the dotfiles dir.
+                                            "Tangle org-mode files from the source dir to the dotfiles dir.
 
 By default, build will only tangle files that have changed since last run."
-  (interactive "P")
-  (run-hooks 'tangld-prebuild-hooks)
-  (let ((source-dir (alist-get 'source tangld-project-dirs))
+                                            (interactive "P")
+                                            (run-hooks 'tangld-prebuild-hooks)
+                                            (let ((source-dir (alist-get 'source tangld-project-dirs))
 	(dotfiles-dir (alist-get 'dotfiles tangld-project-dirs))
 	(files (directory-files-recursively .source "."))
 	(target-file nil))
@@ -257,7 +257,9 @@ By default, build will only tangle files that have changed since last run."
 	     (f-symlink file target-dir))
 	    ((or force (not (file-exists-p target)) (file-newer-than-file-p file target-file))
 	     (tangld--async-tangle-file file .dotfiles)))))
-  (run-hooks 'tangld-postbuild-hooks))
+                                            (run-hooks 'tangld-postbuild-hooks))
+
+;;;; Link type build functins
 
 ;;;; Install - tangld-install
 
