1690803a421b6912be1055f60f2687152971684f
Luis Henriquez-Perez <luis@luishp.xyz>
2021-04-04 19:55:10 -0400

    Update tangld

--- tangld.el
+++ tangld.el
@@ -255,7 +255,8 @@ By default, build will only tangle files that have changed since last run."
 ;;;; Link type build functins
 
 (defun tangld--link-type-build (file)
-  (funcall (intern (format "tangld--link-type-%s-build")) file))
+  (let ((build-fn (intern (format "tangld--link-type-%s-build"))))
+    (funcall file)))
 
 (defun tangld--link-type-direct-build (file)
   "Tangle files into the build directory."
