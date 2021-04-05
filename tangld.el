4f5e9beb68c888ebc1ba40039b208d1b6ab47dd4
Luis Henriquez-Perez <luis@luishp.xyz>
2021-04-05 13:16:49 -0400

    Update tangld

--- tangld.el
+++ tangld.el
@@ -289,10 +289,14 @@ By default, build will only tangle files that have changed since last run."
 
 (defun tangld--link-type-install (file)
   "Apply appropriate install action based on `tangld-install-type'."
+  (let* ((tangld-install-type (or tangld-install-type 'default)))
+    (funcall ))
   (if (null tangld-install-type)
-      )
-  (let (())
-    (funcall install-fn file)))
+      (tangld--link-type-default-install)
+    (let (())
+      (funcall install-fn file))
+    )
+  )
 
 (defun tangld--link-type-direct-install (file)
   "Move FILE from build-dir to system-dir."
