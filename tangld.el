9ed33ffc097773d84f3c3b27364f0709a7fa3959
Luis Henriquez-Perez <luis@luishp.xyz>
2021-04-05 09:41:39 -0400

    Update tangld

--- tangld.el
+++ tangld.el
@@ -327,9 +327,8 @@ By default, build will only tangle files that have changed since last run."
 
 (defun tangld--link-type-link-clean (file)
   "Remove file created by clean link type."
-  (let (target)
-    (when (f-symlink-p target)
-      (f-delete target))))
+  (when (f-symlink-p target)
+    (f-delete target)))
 
 (defun tangld--link-type-stow-clean (file)
   "Remove file created by direct.")
