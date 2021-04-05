d5b9eac6184f2da67f3ed89cd433fd01034c0590
Luis Henriquez-Perez <luis@luishp.xyz>
2021-04-05 18:42:46 -0400

    Update tangld

--- tangld.el
+++ tangld.el
@@ -175,9 +175,9 @@ That is, when the target file either does not exist or is older than the source 
   "Return the tangle target of link-type based on FILE."
   (f-expand (f-relative file source-dir) target-dir))
 
-(defun tangld-default-build-fn (file source target)
+(defun tangld-default-build-fn (file source-dir target-dir)
   "Build FILE from SOURCE to TARGET."
-  (let ((target-file))
+  (let ((target (f-expand (f-relative file source-dir) target-dir)))
     (cond ((file-ext-p file "org")
 	   (tangld--tangle file target tangld--lazy-tangle-p))
 	  (t
