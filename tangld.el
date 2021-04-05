cd5007f3fda2b17f6cd137795291223677ab0ac7
Luis Henriquez-Perez <luis@luishp.xyz>
2021-04-05 09:40:50 -0400

    Update tangld

--- tangld.el
+++ tangld.el
@@ -285,7 +285,8 @@ By default, build will only tangle files that have changed since last run."
 
 ;;;; Install - tangld-install
 
-(defun tangld--link-type-install (file))
+(defun tangld--link-type-install (file)
+  (funcall ))
 
 (defun tangld--link-type-direct-install (file)
   "Move FILE from build-dir to system-dir."
