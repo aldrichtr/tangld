d17cba07d1cd0e052ef3a3d30dbb6c7511710354
Luis Henriquez-Perez <luis@luishp.xyz>
2021-04-05 22:25:22 -0400

    Update tangld

--- tangld.el
+++ tangld.el
@@ -195,41 +195,6 @@ FORCE is enabled, tangle no matter what."
 
 ;;;; Initialization - tangld-init
 
-(defun tangld-init ()
-  "Setup a new tangld project"
-  (interactive)
-
-  (let-alist (tangld--expanded-project-dir-paths tangld-project-dirs)
-    (cond ((not (f-exists-p .root)) nil)
-	  (tangld-inhibit-init-if-exists
-	   (error (format "Aborted init in %s" .root)))
-	  ((y-or-n-p (format "WARNING: this will overwrite your project in %s continue?" .root))
-	   (f-delete .root t))
-	  (t
-	   (tangld--message "overwriting %S" .root)))
-
-    ;; either it's a new directory or the old one was deleted
-    (tangld--message "creating directories in %s" .root)
-    (mapc #'f-mkdir (mapcar #'cdr (tangld--expanded-project-dir-paths tangld-project-dirs)))
-
-    ;; add the lib directory to the list of babel libraries
-    (when tangld-add-project-lib-dir-on-init
-      (add-to-list 'tangld-babel-library-dirs .lib))
-
-    ;; initialize the version control (git init)
-    (when tangld-init-vc-on-init
-      (tangld-init--init-vc .root))
-    (tangld--message "initialized new tangld project in %s" .root)))
-
-(defun tangld-init--init-vc (&optional vc-root-dir)
-  "Initialize the project using magit."
-  (tangld--with-project-dirs
-    (or vc-root-dir (setq vc-root-dir .root))
-    (tangld--message "initializing git repo in %s" vc-root-dir)
-    (if (featurep 'magit)
-	(magit-call-git "init" (magit-convert-filename-for-git (expand-file-name vc-root-dir)))
-      (message "Magit package not found"))))
-
 ;;;; Configuration - tangld-config
 
 (defun tangld-config (&optional type)
