e0a8dac0ce91ee2f0363b167f1b56bbd44744b90
Luis Henriquez-Perez <luis@luishp.xyz>
2021-04-05 22:21:12 -0400

    Update tangld

--- tangld.el
+++ tangld.el
@@ -261,35 +261,6 @@ build type i.e. OS specific, shell options alternate install directory, etc."
       (tangld--message "Build options saved to '%s'" config-file))))
 
 ;;;; Clean - tangld-clean
-
-(defun tangld--link-type-clean (file)
-  "Apply appropriate clean action to FILE based on `tangld-install-type'."
-  (let* ((tangld-install-type (or tangld-install-type 'default))
-	 (clean-fn (intern (format "tangld--link-type-%s-clean" tangld-install-type))))
-    (funcall clean-fn file)))
-
-(defun tangld--link-type-direct-clean (file)
-  "Remove FILE created by direct."
-  (f-delete file (tangld--target-file file 'direct)))
-
-(defun tangld--link-type-link-clean (file)
-  "Remove symlink created by clean link type."
-  (let ((target (tangld--target-file file 'link)))
-    (when (f-symlink-p target)
-      (f-delete target))))
-
-(defun tangld--link-type-stow-clean (file)
-  "Remove FILE created by direct link type."
-  (tangld--message "Not yet implemented."))
-
-(defun tangld--link-type-default-clean (_)
-  (tangld--message "Does nothing."))
-
-(defun tangld-clean ()
-  "Remove any symlinks corresponding to files in dotfiles-dir."
-  (interactive)
-  (mapc #'tangld--link-type-clean (directory-files-recursively (alist-get 'source tangld-project-dirs) ".")))
-
 ;;;; Check - tangld-check
 
 (defun tangld-check ()
