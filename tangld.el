321fa373874b2d32f7ada6e6bcbbad2f165f92a5
Luis Henriquez-Perez <luis@luishp.xyz>
2021-04-05 22:15:20 -0400

    Update tangld

--- tangld.el
+++ tangld.el
@@ -268,37 +268,6 @@ build type i.e. OS specific, shell options alternate install directory, etc."
       (tangld--ignore (f-write config-file))
       (tangld--message "Build options saved to '%s'" config-file))))
 
-;;;; Build - tangld-build
-
-(defun tangld--link-type-build (file)
-  "Apply appropriate build action based on `tangld-install-type'."
-  (let* ((tangld-install-type (or tangld-install-type 'default))
-	 (build-fn (intern (format "tangld--link-type-%s-build" tangld-install-type))))
-    (funcall build-fn file)))
-
-(defun tangld--link-type-direct-build (file)
-  "Tangle file to the build directory."
-  (funcall tangld--build-fn file 'source 'build))
-
-(defun tangld--link-type-link-build (file)
-  "Tangle file to install-root-dir."
-  (funcall tangld--build-fn file 'source 'install))
-
-(defun tangld--link-type-stow-build (file)
-  "Tangle file to build directory."
-  (funcall tangld--build-fn file 'source 'build))
-
-(defalias 'tangld--link-type-default-build 'tangld--link-type-link-build)
-
-(defun tangld-build (&optional force)
-  "Tangle org-mode files from the source dir to the dotfiles dir.
-
-By default, build will only tangle files that have changed since last run."
-  (interactive "P")
-  (run-hooks 'tangld-prebuild-hooks)
-  (tangld--let* ((tangld--lazy-tangle force))
-    (mapc #'tangld--link-type-build (directory-files-recursively .source "."))))
-
 ;;;; Install - tangld-install
 
 (defun tangld--link-type-install (file)
