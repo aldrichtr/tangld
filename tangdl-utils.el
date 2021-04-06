0fe4012762eb47c6088c396676dd70b3034b20b9
Luis Henriquez-Perez <luis@luishp.xyz>
2021-04-06 10:42:33 -0400

    Add some variables for tangling customization.

--- tangdl-utils.el
+++ tangdl-utils.el
@@ -17,33 +17,28 @@
 ;; document, build, and install configuration files, scripts and other
 ;; files on a system.  More details are available in the README.org file.
 
-(defcustom tangld-tangle-fn )
+(defcustom tangld-tangle-fn #'tangld--tangle
+  "Function that returns a function that tangles a file to a target.
+The function should accept two arguments, the file and the target. The function
+should return (OUTCOME FILE TARGET), where OUTCOME is non-nil when tangling has
+been successful.")
 
-(defun tangld--tangle ()
-  (require 'org)
-  (require 'ob-tangle)
-  (let ((org-babel-confirm-evaluate nil)
-	(gc-cons-threshold most-positive-fixnum)
-	(org-babel-default-header-args '((:tangle . "yes") (:results . "silent"))))
-    (list (ignore-errors (org-babel-tangle-file ,file ,target))
-	  ,file
-	  ,target)))
+(defcustom tangld-tangle-result-fn #'tangld--tangle-outcome
+  "Function that outputs a result based on tangling.")
 
-(defun tangld--tangle-outcome (result)
-  (cl-destructuring-bind (outcome file target) result
-    (message "%s in tangling %s to %s" (if outcome "Succeded" "Failed") file target)))
-
-(defun tangld--target-file (file source-dir target-dir)
-  "Return the tangle target of link-type based on FILE."
-  (f-expand (f-relative file source-dir) target-dir))
-
-(defun tangld--message (format-string &rest args)
-  "Display message if `tangld-verbose-p' is non-nil."
-  (when tangld-verbose-p (message (format "[tangld] %s" (format format-string args)))))
+(defun tangld-tangle-fn (file target)
+  "Return a lambda tha tangles FILE to TARGET."
+  `(lambda ()
+     (require 'org)
+     (require 'ob-tangle)
+     (let ((org-babel-confirm-evaluate nil)
+	   (gc-cons-threshold most-positive-fixnum)
+	   (org-babel-default-header-args '((:tangle . "yes") (:results . "silent"))))
+       (list (ignore-errors (org-babel-tangle-file ,file ,target)) ,file ,target))))
 
 (defun tangld--async-tangle-file (file target)
   "Asynchronously tangle FILE to TARGET."
-  (async-start #'fn #'tangld--tangle-outcome))
+  (async-start (funcall tangld-tangle-fn file target) #'tangld--tangle-outcome))
 
 (defun tangld--tangle (file target &optional force)
   "Tangle FILE into PROJECT-DIR.
@@ -53,7 +48,19 @@ FORCE is enabled, tangle no matter what."
     (tangld--message "tangling %s -> %s")
     (tangld--async-tangle-file file target)))
 
-;; It is far more useful to have access to the full paths than the components.
+(defun tangld--tangle-outcome (result)
+  "Output the result of tangling in the messages buffer."
+  (cl-destructuring-bind (outcome file target) result
+    (tangld--message "%s in tangling %s to %s" (if outcome "Succeded" "Failed") file target)))
+
+(defun tangld--target-file (file source-dir target-dir)
+  "Return the tangle target of link-type based on FILE."
+  (f-expand (f-relative file source-dir) target-dir))
+
+(defun tangld--message (format-string &rest args)
+  "Display message if `tangld-verbose-p' is non-nil."
+  (when tangld-verbose-p (message (format "[tangld] %s" (format format-string args)))))
+
 (defun tangld--expanded-project-dir-paths ()
   "Return `tangld-project-dirs' with values all expanded."
   (let ((expanded nil)
