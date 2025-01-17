b992f805fb52f2ac49c718d430e8697098dadbbc
Timothy Aldrich <timothy.r.aldrich@gmail.com>
2025-01-17 17:56:04 -0500

    tests: Created tests for logging and checksums using buttercup

---
+++ tmp/tangld.el
@@ -0,0 +1,39 @@
+;;; tangld.el --- literate config development environment -*- lexical-binding: t; -*-
+;;
+
+;;; License:
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;; A Literate Programming Environment for configuration files and scripts
+
+;; tangld is an Emacs package that provides "dotfiles management" features
+;; using Literate Programming paradigms.  Using org-mode files with source
+;; blocks and the tangle functionality, Emacs can be used as an IDE to
+;; document, build, and install configuration files, scripts and other
+;; files on a system.  More details are available in the README.org file.
+
+;;; Code:
+
+(require 'ob-tangle)
+
+(require 'tangld-core)
+(require 'tangld-log)
+(require 'tangld-checksum)
+(require 'tangld-build)
+
+(provide 'tangld)
+
+;;; tangld.el ends here
